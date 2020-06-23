8e6d12c8259d7a4d189b19d4b2b72b2e0d4cc2a2
ardanclassic <ardan.classic@gmail.com>
2020-06-23 11:02:21 +0700

    second commit

--- js/url_caches.js
+++ js/url_caches.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'noah_caches';
-let urlsToCache = [
+export const urlCache = [
     '/',
     '/manifest.json',
     '/service-worker.js',
@@ -42,45 +41,5 @@ let urlsToCache = [
     'assets/icon/up.png',
     'assets/gif/drown.gif',
     'assets/gif/sail.gif',
+
 ];
-
-self.addEventListener('install', function(event){
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
-})
-
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName){
-					if(cacheName != CACHE_NAME){	
-						console.log("ServiceWorker: cache " + cacheName + " dihapus");
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-})
-
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request, {cacheName:CACHE_NAME})
-		.then(function(response) {
-			if(response){
-				// console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-				return response;
-			}
-			
-			// console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
-			return fetch(event.request);
-		})
-	);
-});
-
